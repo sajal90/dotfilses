dbcc6494656af8a98e2246a8d9cbaf20142d9f67
sajal90 <sajal2005gupta@gmail.com>
2023-09-08 19:58:16 +0530

    install script

--- install_Hyprland.sh
+++ install_Hyprland.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #
 mkdir ~/git
+mkdir ~/.config
 cd ~/git
 
-sudo pacman -Syyu git neovim 
+sudo pacman -Syyu --needed base-devel git alacritty neovim waybar firefox ttf-jetbrains-mono-nerd
+
+git clone https://github.com/Morganamilo/paru
+cd paru
+makepkg -si
+
+
+paru -S hyprland-git ttf-merriweather
+
+
+git clone https://github.com/sajal90/dotfiles
+cd dotfiles
+
+cp -r hypr waybar neovim alacritty ~/.config
 

6a0d5b9d7fe3fa371c0a5b561dbb9a082d5e0009
sajal90 <sajal2005gupta@gmail.com>
2023-09-12 12:30:54 +0530

    updated script

--- install_Hyprland.sh
+++ install_Hyprland.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #
-mkdir ~/git
 mkdir ~/.config
-cd ~/git
 
-sudo pacman -Syyu --needed base-devel git alacritty neovim waybar firefox ttf-jetbrains-mono-nerd
+sudo pacman -Syyu --needed base-devel alacritty neovim waybar firefox ttf-jetbrains-mono-nerd
 
 git clone https://github.com/Morganamilo/paru
 cd paru
